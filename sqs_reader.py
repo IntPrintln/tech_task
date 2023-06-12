4abb65fcf11686cf0357195a5120df7b361140ed
Andrey_DO <32335469+IntPrintln@users.noreply.github.com>
2023-06-12 11:26:58 +0300

    Update sqs_reader.py

--- sqs_reader.py
+++ sqs_reader.py
@@ -3,7 +3,7 @@ import boto3
 def read_messages():
     # Connect to SQS
     sqs = boto3.resource('sqs', region_name='us-east-1')
-    queue_url = 'your_queue_url'
+    queue_url = 'https://us-east-1.console.aws.amazon.com/sqs/v2/home?region=us-east-1#/queues/https%3A%2F%2Fsqs.us-east-1.amazonaws.com%2F163825456495%2Ftech_task_SQSqueue'
     
     # Retrieve messages
     queue = sqs.Queue(queue_url)
